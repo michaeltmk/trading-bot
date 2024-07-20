4f02fd910758cf963c1ecd732c839beb32179464
michael tse <tse.man.kit.michael@gmail.com>
2024-07-21 00:32:50 +0800

    feat: Add base class for loading config files

---
+++ trading_bot/src/config/base_class.py
@@ -0,0 +1,10 @@
+import yaml
+import os
+
+class ConfigBaseClass:
+    
+    @staticmethod
+    def load_config(config_file):
+        with open(config_file, "r") as file:
+            config = yaml.safe_load(file)
+        return config
---
+++ trading_bot/src/config/bot_config.py
@@ -0,0 +1,6 @@
+from trading_bot.src.config.base_class import ConfigBaseClass
+from pydantic import BaseModel
+
+
+class BotConfigModel(BaseModel):
+    
