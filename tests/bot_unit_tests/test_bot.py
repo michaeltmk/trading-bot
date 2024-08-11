c7f8e93d1e8a755b183de01942dc54e0120c8c77
michael tse <tse.man.kit.michael@gmail.com>
2024-08-11 16:03:48 +0800

    test: update test_signal_generator method

--- tests/bot_unit_tests/test_bot.py
+++ tests/bot_unit_tests/test_bot.py
@@ -3,23 +3,22 @@ from trading_bot.bot import signal_generator
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import pytest
 import asyncio
+from trading_bot import utilities
 
 @pytest.fixture
 def anyio_backend():
     return 'asyncio'
 
 async def _test_bot(anyio_backend):
-    signal_generator_1 = signal_generator()
-    signal_generator_1.append('ma_in_sequence')
-    signal_generator_1.append('trailing_stop_orders')
+    sg1_config = utilities.config_utilities.load_strategies_config('config/sg1_strategies_config.yaml')
+    signal_generator_1 = signal_generator(sg1_config)
 
     async with AsyncScheduler() as scheduler:
         scheduler_1 = scheduler()
         scheduler_1.append(60, signal_generator_1)
         scheduler_1.run()
 
-def test_main2():
-    signal_generator_1 = signal_generator()
-    signal_generator_1.append('ma_in_sequence')
-    signal_generator_1.append('trailing_stop_orders')
-    signal_generator_1.run()
+def test_signal_generator():
+    sg1_config = utilities.config_utilities.load_strategies_config('config/sg1_strategies_config.yaml')
+    signal_generator_1 = signal_generator(sg1_config)
+    signal_generator_1.run({})
